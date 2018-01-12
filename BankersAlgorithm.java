32ba469050e2e9eade2ae29d504b3d2170006c99
Igor L. R. Alves <igor.lucio@ufv.br>
2018-01-12 02:00:21 -0200

    Update Java version

--- BankersAlgorithm.java
+++ BankersAlgorithm.java
@@ -8,30 +8,30 @@ public class BankersAlgorithm {
     private int need[][],allocate[][],max[][],avail[][],np,nr;
     
     private void input(){
-     Scanner sc = new Scanner(System.in);
-     System.out.print("Enter no. of processes and resources: ");
-     np=sc.nextInt();  //no. of processes
-     nr=sc.nextInt();  //no. of resources
-     need=new int[np][nr];  //initializing arrays
-     max=new int[np][nr];
-     allocate=new int[np][nr];
-     avail=new int[1][nr];
-     
-     System.out.println("Enter allocation matrix: ");
-     for(int i=0;i<np;i++)
-          for(int j=0;j<nr;j++)
-         allocate[i][j]=sc.nextInt();  //allocation matrix
-      
-     System.out.println("Enter max. matrix: ");
-     for(int i=0;i<np;i++)
-          for(int j=0;j<nr;j++)
-         max[i][j]=sc.nextInt();  //max matrix
-      
-        System.out.println("Enter available matrix: ");
-        for(int j=0;j<nr;j++)
-            avail[0][j]=sc.nextInt();  //available matrix
-        
-        sc.close();
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter no. of processes and resources: ");
+         np=sc.nextInt();  //no. of processes
+         nr=sc.nextInt();  //no. of resources
+         need=new int[np][nr];  //initializing arrays
+         max=new int[np][nr];
+         allocate=new int[np][nr];
+         avail=new int[1][nr];
+
+         System.out.println("Enter allocation matrix: ");
+         for(int i=0;i<np;i++)
+              for(int j=0;j<nr;j++)
+             allocate[i][j]=sc.nextInt();  //allocation matrix
+
+         System.out.println("Enter max. matrix: ");
+         for(int i=0;i<np;i++)
+              for(int j=0;j<nr;j++)
+             max[i][j]=sc.nextInt();  //max matrix
+
+            System.out.println("Enter available matrix: ");
+            for(int j=0;j<nr;j++)
+                avail[0][j]=sc.nextInt();  //available matrix
+
+            sc.close();
     }
     
     //calculate the need matrix
